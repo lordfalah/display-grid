5fc8cc9375097cae18d923b62f5bfa7d3c71dd27
kervinpala <kervinpala@gmail.com>
2021-03-27 16:44:40 +0700

    create nav bar native finish v.1

--- latihan-4/studyCase.js
+++ latihan-4/studyCase.js
@@ -2,21 +2,26 @@ function slideShow(){
     const getBurger = document.querySelector(".burger");
     const getLink = document.querySelector("nav .linkNav");
     const getAllLink = document.querySelectorAll("nav .linkNav li");
-    // console.log(getAllLink);
+    const getBody = document.querySelector("body");
+
+    console.log(getBody);
     getBurger.addEventListener("click", function(){
         // console.log(this)
         getLink.classList.toggle("slideFade");
 
-        if(getLink.classList.contains("slideFade")){
-            console.log("MUNCUL");
-            getAllLink.forEach((link, index) =>{
-                link.style.animation = `allLinkFade 0.90s ease ${index / (5 + 0.2)}s`;
-                // console.log(index / 5)
-            })
-        
-        }else{
-            console.log("ENDDD");
-        }
+        getAllLink.forEach((link, index) =>{
+            if(getLink.classList.contains("slideFade")){
+                console.log("ada");
+                link.style.animation = `allLinkFade 1s  forwards ${index / (7 + 0.9)}s`;
+                getBody.classList.add("warna");
+
+            }else{
+                console.log("HILANG");
+                getBody.classList.remove("warna");
+                link.style.animation = `deleteFade 0.80s ease-out ${index / 5}s`;
+            }
+        })
+
     });
 
 }
